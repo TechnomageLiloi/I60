5c3a4d97942350be600c98382c4a5152b86d75b9
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-02-21 05:28:02 +0200

    [0.00.010] Add autoload register f-n.

--- Application.php
+++ Application.php
@@ -6,6 +6,30 @@ use Rune\Application\General as GeneralApplication;
 
 class Application extends GeneralApplication
 {
+    const PREFIX = 'Liloi\I60';
+
+    public function __construct(array $config)
+    {
+        parent::__construct($config);
+
+        spl_autoload_register(function ($className) {
+            if(!str_starts_with($className, self::PREFIX))
+            {
+                return;
+            }
+
+            $className = str_replace(self::PREFIX, '', $className);
+            $className = str_replace('\\', '/', $className);
+
+            $file = __DIR__ . $className . '.php';
+
+            if(file_exists($file))
+            {
+                require_once $file;
+            }
+        });
+    }
+
     public function apiLayout(): array
     {
         return [
--- Documents/ToDo.md
+++ Documents/ToDo.md
@@ -3,7 +3,7 @@
 -[x] Bind application with database
 -[x] Add menu block
 -[x] Add road domain
--[ ] Add autoload register f-n
+-[x] Add autoload register f-n
 -[ ] Add exceptions
 -[ ] Update Show API method
 -[ ] Update Create API method
