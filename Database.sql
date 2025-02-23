ca8371baa66d0d3451388c7b4db5d6c1faa03afb
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-02-23 14:36:14 +0200

    [0.02.000] Add problems database table.

--- Database.sql
+++ Database.sql
@@ -10,3 +10,11 @@ create table i60_road
 		primary key (key_road)
 );
 
+create table i60_problems
+(
+	key_problem timestamp not null,
+	title varchar(500) not null,
+	constraint i60_problems_pk
+		primary key (key_problem)
+);
+
