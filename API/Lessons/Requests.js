b79270ed95158a78f4b2af1eb0970e744a319c3d
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-04-03 10:57:16 +0300

    [0.03.034] Add lessons API methods (in first approx.)

---
+++ API/Lessons/Requests.js
@@ -0,0 +1,87 @@
+Requests.Lessons = {
+    getCollection: function ()
+    {
+        API.request('Lessons.Collection', {
+
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    show: function (key)
+    {
+        API.request('Lessons.Show', {
+            'key': key
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    create: function ()
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        API.request('Lessons.Create', {
+            'debug': true
+        }, function (data) {
+            Requests.Lessons.getCollection();
+        }, function () {
+
+        });
+    },
+
+    remove: function (key)
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        API.request('Lessons.Remove', {
+            'key': key
+        }, function (data) {
+            Requests.Lessons.getCollection();
+        }, function () {
+
+        });
+    },
+
+    edit: function (key)
+    {
+        API.request('Lessons.Edit', {
+            'key': key
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    save: function (key)
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        const jq_block = $('#ticket-edit');
+        API.request('Lessons.Save', {
+            'key': key,
+            'title': jq_block.find('[name="title"]').val(),
+            'goal': jq_block.find('[name="goal"]').val(),
+            'status': jq_block.find('[name="status"]').val(),
+            'program': jq_block.find('[name="program"]').val()
+        }, function (data) {
+            Requests.Lessons.getCollection();
+        }, function () {
+
+        });
+    }
+}
