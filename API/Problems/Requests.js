393782dd871f666495243118b7858cb047cf824e
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-04-03 11:01:02 +0300

    [0.03.035] Add problems API methods (in first approx.)

---
+++ API/Problems/Requests.js
@@ -0,0 +1,87 @@
+Requests.Problems = {
+    getCollection: function ()
+    {
+        API.request('Problems.Collection', {
+
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    show: function (key)
+    {
+        API.request('Problems.Show', {
+            'key': key
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    create: function ()
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        API.request('Problems.Create', {
+            'debug': true
+        }, function (data) {
+            Requests.Problems.getCollection();
+        }, function () {
+
+        });
+    },
+
+    remove: function (key)
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        API.request('Problems.Remove', {
+            'key': key
+        }, function (data) {
+            Requests.Problems.getCollection();
+        }, function () {
+
+        });
+    },
+
+    edit: function (key)
+    {
+        API.request('Problems.Edit', {
+            'key': key
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    save: function (key)
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        const jq_block = $('#ticket-edit');
+        API.request('Problems.Save', {
+            'key': key,
+            'title': jq_block.find('[name="title"]').val(),
+            'goal': jq_block.find('[name="goal"]').val(),
+            'status': jq_block.find('[name="status"]').val(),
+            'program': jq_block.find('[name="program"]').val()
+        }, function (data) {
+            Requests.Problems.getCollection();
+        }, function () {
+
+        });
+    }
+}
