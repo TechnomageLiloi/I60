9703b1689d08c7bc2a09b08ab223735de5e4d7ce
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-04-10 03:35:30 +0300

    [0.00.003] Rise I60 system.

---
+++ API/Layout.tpl
@@ -0,0 +1,4 @@
+<link href="<?php echo $config['root']; ?>/API/Style.css" rel="stylesheet" />
+<div id="layout" style="text-align: center;height: 100%;">
+    <script>//Requests.show();</script>
+</div>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Liloi\I60;
+
+include_once __DIR__ . '/RuneFramework.phar';
+include_once __DIR__ . '/Application.php';
+
+$private = json_decode(file_get_contents('./Config/Private.json'), true);
+
+$config = array_merge([
+    'title' => 'Interstate 60',
+    'start' => 'Requests.layout();',
+    'scripts' => [
+        $private['root'] . '/Requests.js',
+    ],
+    'prefix' => 'i60_'
+], $private);
+
+$app = new Application($config);
+
+echo $app->compile();
